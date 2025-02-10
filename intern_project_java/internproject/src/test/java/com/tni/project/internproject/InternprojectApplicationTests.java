8dec23bbf182df3fd7543e91dcf6ffb5a0f983ae
PaisanKjn <bank200074@gmail.com>
2025-02-10 16:43:47 +0900

    Initialize project

---
+++ intern_project_java/internproject/src/test/java/com/tni/project/internproject/InternprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tni.project.internproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
